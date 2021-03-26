a169375b2926ba5396dd0dc0da62e46717def397
Roi-Livne <roilivne14@gmail.com>
2021-03-26 18:34:29 +0300

    card image and text

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,37 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                color: Colors.red,
-                width: 100,
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                    'https://media-exp1.licdn.com/dms/image/C4D03AQFAENY1qZ6kHg/profile-displayphoto-shrink_800_800/0/1546935334117?e=1622073600&v=beta&t=-zTcX-y6HhaXtEQX14AguTJkVdJc6eY7nqJKeZWMikI'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.yellow,
-                    height: 100,
-                    width: 100,
-                  ),
-                  Container(
-                    color: Colors.lightGreen,
-                    height: 100,
-                    width: 100,
-                  ),
-                ],
+              Text(
+                'Roi Livne',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Container(
-                color: Colors.blue,
-                width: 100,
+              Text(
+                'FULL STACK DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ],
           ),
-        ),
+        )),
       ),
     );
   }
